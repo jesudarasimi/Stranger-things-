3b9a63683448c72709f8f15386d84f0617fd6a02
techBRO.D <107814492+jesudarasimi@users.noreply.github.com>
2022-06-27 00:56:52 +0100

    Add files via upload
    This is most likely my most creative project for now

---
+++ index.js
@@ -0,0 +1,8 @@
+function displayGreeting(name, salutation='welcome to our page') 
+{
+  alert(`${salutation}, ${name}`);
+}
+
+displayGreeting('Dara')
+
+displayGreeting('Dara?','Are you ready to see this')
---
+++ script.js
@@ -0,0 +1,7 @@
+function displayGreeting(name, salutation='Did you like the previous season') 
+{
+  alert(`${salutation}, ${name}`);
+}
+
+displayGreeting('Dara?'
+
